2466cda2efe24e2ef9c33637e196a2759b461b32
Anna Escolaro <annaescolaro@hotmail.com>
2023-03-28 19:08:07 -0300

    exercicios 3

--- modulo-fundamentos/secao3-dia1-exercicios/script.js
+++ modulo-fundamentos/secao3-dia1-exercicios/script.js
@@ -1,23 +1,52 @@
-let a = 1;
-let b = 3;
-let c = 5;
+// let a = 1;
+// let b = 3;
+// let c = 5;
 
-let soma = a + b;
-console.log(soma);
+// let soma = a + b;
+// console.log(soma);
 
-let multiply = a * b;
-console.log(multiply);
+// let multiply = a * b;
+// console.log(multiply);
 
-if (b>a) {
-    console.log('b é maior que a');
+// if (b>a) {
+//     console.log('b é maior que a');
+// } else {
+//     console.log('b é menor que a');
+// }
+
+// if (b>a && b>c)  {
+//     console.log('b é o maior');
+// } else if (c>a && c>3) {
+//     console.log('c é o maior');
+// } else {
+//     console.log('a é o maior');
+// }
+
+let valor = -5;
+
+if (valor > 0) {
+    console.log('positive');
+} else if (valor < 0) {
+    console.log('negative');
 } else {
-    console.log('b é menor que a');
-}
+    console.log('zero');
+};
 
-if (b>a && b>c)  {
-    console.log('b é o maior');
-} else if (c>a && c>3) {
-    console.log('c é o maior');
+
+let angulo1 = 90;
+let angulo2 = 45;
+let angulo3 = 46;
+
+let somaAngulos = angulo1 + angulo2 + angulo3;
+
+let somaAngulosPositivos = angulo1 > 0 && angulo2 > 0 && angulo3 > 0;
+
+if(somaAngulosPositivos){
+    if(somaAngulos === 180) {
+        console.log(true);
 } else {
-    console.log('a é o maior');
+        console.log(false);
+};
+} else {
+        console.log('erro');
 }
