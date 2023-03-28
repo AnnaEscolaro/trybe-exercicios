f0ddd67c75bebe19ffe87915533a183d6126a2a9
Anna Escolaro <annaescolaro@hotmail.com>
2023-03-28 18:50:03 -0300

    exercicios 2

---
+++ modulo-fundamentos/secao3-dia1-exercicios/script.js
@@ -0,0 +1,23 @@
+let a = 1;
+let b = 3;
+let c = 5;
+
+let soma = a + b;
+console.log(soma);
+
+let multiply = a * b;
+console.log(multiply);
+
+if (b>a) {
+    console.log('b é maior que a');
+} else {
+    console.log('b é menor que a');
+}
+
+if (b>a && b>c)  {
+    console.log('b é o maior');
+} else if (c>a && c>3) {
+    console.log('c é o maior');
+} else {
+    console.log('a é o maior');
+}
