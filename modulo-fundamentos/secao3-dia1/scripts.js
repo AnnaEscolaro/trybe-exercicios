3298b45cfadcc058d00dbb587f6bf8c8388f216f
Anna Escolaro <annaescolaro@hotmail.com>
2023-03-29 18:20:49 -0300

    ajustes

---
+++ modulo-fundamentos/secao3-dia1/scripts.js
@@ -0,0 +1,34 @@
+// let status = '';
+
+// switch (status) {
+//     case 'aprovada':
+//         console.log('parabéns');
+//         break;
+
+//     case 'lista':
+//         console.log('lista');
+//         break;
+
+//     case 'reprovada':
+//         console.log('reprovada');
+//         break;
+
+//     default:
+//         console.log('info incorreta');
+// }
+
+
+
+
+
+
+// let temperatura = 40;
+
+// let farenheit = temperatura/5*9+32
+
+// console.log(farenheit);
+
+
+
+
+
