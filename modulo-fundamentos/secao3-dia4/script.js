f7afa9aa0dca4ab162d702ec2cdfac8b4478d120
Anna Escolaro <annaescolaro@hotmail.com>
2023-04-04 16:14:26 -0300

    exercicios

---
+++ modulo-fundamentos/secao3-dia4/script.js
@@ -0,0 +1,33 @@
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+};
+console.log('Bem-vinda, ' + info.personagem);
+
+info['recorrente'] = 'Sim';
+console.log(info);
+
+for (key in info) {
+    console.log(key);
+};
+
+for (index in info) {
+    console.log(info[index]);
+}
+
+let newInfo = {
+    personagem: 'Tio Patinhas',
+    origem: 'Christmas on Bear Mountain, Dells Four Color Comics #178',
+    nota: 'O último MacPatinhas',
+    recorrente: 'Sim',
+};
+
+// Com o template literals
+console.log(`Primeira linha;
+Segunda linha;
+Terceira linha;`
+);
+
+// Sem o template literals:
+console.log('Primeira linha;\n' + 'Segunda linha;\n' + 'Terceira linha;\n');
